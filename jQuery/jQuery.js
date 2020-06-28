7d96d4be16e35a760712d902dd117761b6b2750b
CatalystKory <63815719+CatalystKory@users.noreply.github.com>
2020-06-28 10:41:50 -0700

    Added resizable event

--- jQuery/jQuery.js
+++ jQuery/jQuery.js
@@ -4,4 +4,19 @@ $("#clickBait").click(function(){
 
 });
 
-$("#resizable").resizable();
+$("#resizable").resizable(
+    {grid: 50},
+    {alsoResize: "#also"},
+    {resize: function(event, ui){
+       
+        if ($("#resizable").width() == 300) {
+            alert("Approaching Maximum Size");
+        } else if ( $("#resizable").width() > 600){
+            $("#resizable").hide();
+        }
+    }}
+    );
+
+$("#also").resizable({grid:50});
+
+$("#draggable").draggable( {containment: "#parent"});
