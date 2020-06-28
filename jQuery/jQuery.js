6fe969cdf33742150f10f6d585156e9ed80202ae
CatalystKory <63815719+CatalystKory@users.noreply.github.com>
2020-06-28 11:30:07 -0700

    Added jQuery -- Accordion and Sortable

--- jQuery/jQuery.js
+++ jQuery/jQuery.js
@@ -20,3 +20,17 @@ $("#resizable").resizable(
 $("#also").resizable({grid:50});
 
 $("#draggable").draggable( {containment: "#parent"});
+
+$("#draggable2").draggable();
+$("#droppable").droppable({drop: function(event, ui){
+    var droppable = $("#droppable")
+    
+    alert("you dropped your box.")
+    $("#droppable").css("background-color","green");
+    droppable.html("You did it!")
+    
+}});
+
+$("#accordion").accordion();
+
+$("#sortable").sortable();
