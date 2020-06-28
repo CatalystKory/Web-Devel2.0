fd062d3846f28f8b0d6da65c36b6bb1b41661af6
CatalystKory <63815719+CatalystKory@users.noreply.github.com>
2020-06-28 14:01:57 -0700

    Finished HTML/Output windows Update on change

--- CodePlayerProject/script.js
+++ CodePlayerProject/script.js
@@ -1,3 +1,5 @@
+
+// Mouseover
 $(".toggleButton").hover(function() {
     $(this).addClass("highlightedButton")
 }, function(){
@@ -5,7 +7,23 @@ $(".toggleButton").hover(function() {
 }
 );
 
+// Toggle Selected Windows
 $(".toggleButton").click(function(){
     $(this).toggleClass("active");
+    $(this).removeClass("highlightedButton");
 });
+
+
+
+// Set Panel Height/Width
+$(".panel").height($(window).height() - $("#header").height()- 15);
+
+$(".panel").width(($(window).width() / 2) - 10);
+
+// Output window
+$("#htmlPanel").on('change keyup paste', function(){
+    $("iframe").contents().find("html").html($("#htmlPanel").val());
+})
+
+
 
