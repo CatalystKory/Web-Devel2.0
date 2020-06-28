74fd0de90f386841d9714cac58d5ee9fe13c0715
CatalystKory <63815719+CatalystKory@users.noreply.github.com>
2020-06-28 13:02:58 -0700

    Created header, added toggle/hover functions

---
+++ CodePlayerProject/script.js
@@ -0,0 +1,11 @@
+$(".toggleButton").hover(function() {
+    $(this).addClass("highlightedButton")
+}, function(){
+    $(this).removeClass("highlightedButton")
+}
+);
+
+$(".toggleButton").click(function(){
+    $(this).toggleClass("active");
+});
+
