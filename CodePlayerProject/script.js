b99131b273342daa87db37dd4626101578726b52
CatalystKory <63815719+CatalystKory@users.noreply.github.com>
2020-06-29 08:49:23 -0700

    Finished codeplayer

--- CodePlayerProject/script.js
+++ CodePlayerProject/script.js
@@ -11,7 +11,14 @@ $(".toggleButton").hover(function() {
 $(".toggleButton").click(function(){
     $(this).toggleClass("active");
     $(this).removeClass("highlightedButton");
+    var panelId = $(this).attr("id") + "Panel";
+    $("#" + panelId).toggleClass("hidden");
+    var numOfActivePanels = 4 - $(".hidden").length;
+    $(".panel").width(($(window).width() / numOfActivePanels) - 10);
 });
+
+
+
 
 
 
@@ -21,8 +28,20 @@ $(".panel").height($(window).height() - $("#header").height()- 15);
 $(".panel").width(($(window).width() / 2) - 10);
 
 // Output window
-$("#htmlPanel").on('change keyup paste', function(){
-    $("iframe").contents().find("html").html($("#htmlPanel").val());
+
+function updateOutput() {
+    $("iframe").contents().find("html").html("<html><head><style type='text/css'>" + $("#cssPanel").val() + "</style></head><body>" + $("#htmlPanel").val() + "</body></html>");
+
+    document.getElementById("outputPanel").contentWindow.eval($("#javascriptPanel").val());
+     
+};
+
+    updateOutput();
+
+$("textarea").on('change keyup paste', function(){
+    
+    updateOutput();
+
 })
 
 
